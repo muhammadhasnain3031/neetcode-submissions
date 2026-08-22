a25b7f71d0d35be988653ae5790a741ef39d472e
Muhammad Husnain <muhammadhasnain3031@gmail.com>
2026-08-22 10:52:20 +0500

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let map = {};
+        
+        for (let str of strs) {
+            let sortedStr = str.split('').sort().join('');
+            
+            if (!map[sortedStr]) {
+                map[sortedStr] = [];
+            }
+            
+            
+            map[sortedStr].push(str); 
+        } 
+
+        return Object.values(map);
+    }
+}
+
