91149852cc78ebc06db27f8d5ff0d02e94d12265
Muhammad Husnain <muhammadhasnain3031@gmail.com>
2026-09-14 16:32:28 +0500

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,33 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    push(val) {
+        this.stack.push(val);
+
+        if (
+            this.minStack.length === 0 ||
+            val <= this.minStack[this.minStack.length - 1]
+        ) {
+            this.minStack.push(val);
+        }
+    }
+
+    pop() {
+        if (this.stack[this.stack.length - 1] === this.minStack[this.minStack.length - 1]) {
+            this.minStack.pop();
+        }
+
+        this.stack.pop();
+    }
+
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    getMin() {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
